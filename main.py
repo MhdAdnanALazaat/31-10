cc1226c824abf89a1ef743b35e49ae97e14f3a79
Adnan ALazaat <33832766+MhdAdnanALazaat@users.noreply.github.com>
2019-10-31 15:33:02 +0100

    added password hashing

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
 from model import User, db
+import hashlib, uuid
+
 app = Flask(__name__)
 db.create_all() #Erstellt neue datenbank tabellen
 
 @app.route("/")
 def index():
-    email_address = request.cookies.get("email")
-    if email_address:
-     user = db.query(User).filter_by(email=email_address).first()
+    session_token = request.cookies.get("session_token")
+    if session_token:
+     user = db.query(User).filter_by(session_token=session_token).first()
     else:
         user = None
     return render_template("index.html", user=user)
@@ -16,16 +18,24 @@ def login():
     name = request.form.get("user-name")
     email = request.form.get("user-email")
     password=request.form.get("user-password")
+    hashpas=hashlib.sha256(password.encode()).hexdigest()
     #neues objekt vom Typ User (Model)
     #user = User.fetch_one(query=["email", "==", email])
     user = db.query(User).filter_by(email=email).first()
     if not user:
-     user = User(name=name, email=email, password=password)
-     db.add(user)
-     db.commit()
-    #Cookie
-    response = make_response(redirect(url_for('index')))
-    response.set_cookie("email", email)
-    return response
+         user = User(name=name, email=email, password=hashpas)
+         db.add(user)
+         db.commit()
+    if hashpas != user.password:
+        return  "Worng Paswword! Try again!"
+    elif hashpas == user.password:
+        session_token = str(uuid.uuid4())
+        user.session_token = session_token
+        db.add(user)
+        db.commit()
+        #Cookie
+        response = make_response(redirect(url_for('index')))
+        response.set_cookie("session_token", session_token, httponly=True, samesite='Strict')
+        return response
 if __name__ == '__main__':
     app.run()
